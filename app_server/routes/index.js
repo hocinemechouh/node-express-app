832748b7e3032723b85f4def8bc13912598c5dac
hocinemechouh <mechouh2018@gmail.com>
2021-12-03 17:12:09 +0100

    connect with database in external server

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const ctrlMain = require('../controllers/main');
+var express = require("express");
+var router = express.Router();
+var ctrlLocations = require("../controllers/locations");
+var ctrlOthers = require("../controllers/others");
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get("/", ctrlLocations.homelist);
+router.get("/location", ctrlLocations.locationInfo);
+router.get("/location/review/new", ctrlLocations.addReview);
+
+/* Other pages */
+router.get("/about", ctrlOthers.about);
 
 module.exports = router;
